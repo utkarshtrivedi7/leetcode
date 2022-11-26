f414f4373afe5b32d28363cc68b1ec201418faf4
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-11-26 12:52:05 +0530

    Time: 1757 ms (42.65%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -3,8 +3,8 @@ SELECT p.product_id, p.product_name
 FROM Product as p
 WHERE product_id IN
     (
-        SELECT s.product_id
-        FROM Sales as s
-        GROUP BY s.product_id
-        HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31'
+        SELECT product_id
+        FROM Sales
+        GROUP BY product_id
+        HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31'
     )
