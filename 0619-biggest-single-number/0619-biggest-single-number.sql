317b161cf3142d38e3a5aec4ec2c1c3528d1be0e
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-11-29 22:30:49 +0530

    Time: 355 ms (81.72%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 SELECT
-(
+   IFNULL
+   ((
     SELECT num
     FROM MyNumbers
     GROUP BY num
     HAVING COUNT(num)=1
     ORDER BY num DESC
     LIMIT 1
-) as num
+    ), NULL) as num
