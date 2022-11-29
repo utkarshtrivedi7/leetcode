01b82ed009cb847f425b1fa800deb2ff0d5b7d7a
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-11-30 01:50:40 +0530

    Time: 1524 ms (68.49%), Space: 0B (100.00%) - LeetHub

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -2,8 +2,8 @@
 SELECT seller_id
 FROM Sales
 GROUP BY seller_id
-HAVING SUM(PRICE) >= all(
-                            SELECT SUM(PRICE)
+HAVING SUM(price) >= ALL(
+                            SELECT SUM(price)
                             FROM Sales
                             GROUP BY seller_id
                         )
