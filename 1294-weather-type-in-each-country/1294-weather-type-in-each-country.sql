77f131f219e3919857beee509b2c14f72e08a7b1
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-12-05 16:06:28 +0530

    Time: 534 ms (90.93%), Space: 0B (100.00%) - LeetHub

--- 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
+++ 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-SELECT country_name, case when AVG(weather_state*1.0)<=15.0 then 'Cold'
-					when AVG(weather_state*1.0)>=25.0 then 'Hot'
-					else 'Warm'
-			        end AS weather_type
-FROM Countries AS c
-INNER JOIN Weather AS w
-ON c.country_id = w.country_id
-WHERE day BETWEEN '2019-11-01' AND '2019-11-30'
-GROUP BY country_name
+SELECT a.country_name,
+CASE    WHEN AVG(weather_state)<=15 THEN "Cold"
+        WHEN AVG(weather_state)>=25 THEN "Hot"
+        ELSE "Warm" 
+        END as weather_type 
+FROM Countries as a
+INNER JOIN Weather as b
+ON a.country_id=b.country_id
+WHERE b.day BETWEEN "2019-11-01" AND "2019-11-30"
+GROUP BY a.country_id;
