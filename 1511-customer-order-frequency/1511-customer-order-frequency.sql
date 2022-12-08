1e476c935ce25338827c75089598d2b013b8eecc
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-12-08 15:56:00 +0530

    Time: 900 ms (58.57%), Space: 0B (100.00%) - LeetHub

--- 1511-customer-order-frequency/1511-customer-order-frequency.sql
+++ 1511-customer-order-frequency/1511-customer-order-frequency.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-SELECT c.customer_id, c.name
-FROM Customers AS c
-JOIN Orders AS o  ON o.customer_id = c.customer_id
-JOIN Product AS p ON o.product_id = p.product_id
-GROUP BY c.customer_id
-HAVING  SUM(IF(MONTH(o.order_date) = 6, o.quantity, 0) * p.price) >= 100 AND 
-        SUM(IF(MONTH(o.order_date) = 7, o.quantity, 0) * p.price) >= 100;
+SELECT customer_id,name
+FROM Customers 
+JOIN Orders USING(customer_id) 
+JOIN Product USING(product_id)
+GROUP BY customer_id
+HAVING  SUM(IF(MONTH(order_date) = 6, quantity, 0) * price) >= 100 AND 
+        SUM(IF(MONTH(order_date) = 7, quantity, 0) * price) >= 100;
+        
+        
