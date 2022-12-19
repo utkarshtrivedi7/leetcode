146f7e78ae6c49fa197d8a76610d16563092cb64
Utkarsh Trivedi <62072700+utkarshtrivedi7@users.noreply.github.com>
2022-12-19 16:03:23 +0530

    Time: 277 ms (49.04%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,4 +3,4 @@ SELECT
     machine_id, 
     ROUND(SUM(CASE WHEN activity_type = "end" THEN timestamp ELSE -timestamp END)/COUNT(DISTINCT process_id), 3) AS processing_time
 FROM Activity
-GROUP BY 1
+GROUP BY machine_id
